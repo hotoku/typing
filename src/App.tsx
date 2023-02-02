3e87e90eef8143cad394b1e511e625e85e60b8e6
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-02 16:10:33 +0900

    paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Paper, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "10px" }}>
+      <Paper style={{ padding: "10px" }}>
+        <Typography variant="h1" component="div">
+          ほげ
+        </Typography>
+      </Paper>
     </div>
   );
 }
