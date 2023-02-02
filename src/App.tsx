9a1d58839f8082025e3f621868ee748137fc1544
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-02 19:41:58 +0900

    wip: layout is bad

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Paper, Typography } from "@mui/material";
+import Problem from "./components/Problem";
 
 function App() {
   return (
     <div className="App" style={{ padding: "10px" }}>
-      <Paper style={{ padding: "10px" }}>
-        <Typography variant="h1" component="div">
-          ほげ
-        </Typography>
-      </Paper>
+      <Problem />
     </div>
   );
 }
---
+++ src/components/Problem.tsx
@@ -0,0 +1,95 @@
+import { Box, Card, Paper, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+const problemGenerator = (() => {
+  let last = 0;
+  const letters = ["F", "J"];
+
+  return (): string => {
+    const change = Math.random() > 0.3;
+    if (change) {
+      last = (last + 1) % 2;
+    }
+    return letters[last];
+  };
+})();
+
+type HistoryProps = {
+  vals: string[];
+};
+
+function History({ vals }: HistoryProps): JSX.Element {
+  return (
+    <>
+      {vals.map((v, i) => {
+        return (
+          <div>
+            <div
+              key={i}
+              style={{
+                float: "left",
+                width: "1rem",
+                border: "solid black",
+                borderWidth: "1px",
+              }}
+            >
+              {v}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+function Problem(): JSX.Element {
+  const [prob, setProb] = useState(problemGenerator());
+  const [probHistory, setProbHistory] = useState<string[]>([]);
+  const [ansHistory, setAnsHistory] = useState<string[]>([]);
+  const [color, setColor] = useState("");
+
+  const blink = (color: string): void => {
+    setColor(color);
+    setTimeout(() => {
+      setColor("white");
+    }, 100);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent): void => {
+      if (e.metaKey) return;
+      const key = e.key.toUpperCase();
+      const p = prob;
+      const nextProb = problemGenerator();
+
+      if (key !== prob) {
+        blink("#ffeeee");
+      } else {
+        blink("#aaccff");
+      }
+
+      setProb(() => nextProb);
+      setProbHistory((s) => [...s, prob]);
+      setAnsHistory((s) => [...s, key]);
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [prob]);
+
+  return (
+    <Paper style={{ padding: "10px" }}>
+      <Card style={{ margin: "0 auto", width: "10em", background: color }}>
+        <Typography variant="h1" component="div">
+          <Box component="span">{prob}</Box>
+        </Typography>
+      </Card>
+      <History vals={probHistory} />
+      <History vals={ansHistory} />
+    </Paper>
+  );
+}
+
+export default Problem;
